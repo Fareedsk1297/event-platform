eb4d53290175c47559f6d48e85ed16f328dd061f
Fareedsk1297 <shaikhfareed1297@gmail.com>
2024-02-13 23:44:05 +0530

    update code

--- lib/database/index.tsx
+++ lib/database/index.tsx
@@ -1,21 +1,98 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
+
+// const MONGODB_URI = process.env.MONGODB_URI;
+
+// let cached = (global as any).mongoose || { conn: null, promise: null };
+
+// export const connectToDatabase = async () => {
+//   if (cached.conn) return cached.conn;
+
+//   if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
+
+//   cached.promise =
+//     cached.promise ||
+//     mongoose.connect(MONGODB_URI, {
+//       // dbName: "EventApp",
+//       bufferCommands: false,
+//     });
+
+//   cached.conn = await cached.promise;
+
+//   return cached.conn;
+// };
+
+// //this code for checking by me
+// // import mongoose from "mongoose";
+
+// // const MONGODB_URI = process.env.MONGODB_URI;
+
+// if (!MONGODB_URI) {
+//   throw new Error("Something went wrong");
+// }
+
+// let cached = (global as any).mongoose || { conn: null, promise: null };
+
+// // async function connectToDatabase() {
+// //   if (cached.conn) {
+// //     return cached.conn;
+// //   }
+
+// //   if (!cached.promise) {
+// //     const opts = {
+// //       useNewUrlParser: true,
+// //       useUnifiedTopology: true,
+// //     };
+
+// //     cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+// //       console.log("You successfully Connected");
+// //       return mongoose;
+// //     });
+// //   }
+// //   cached.conn = await cached.promise;
+// //   return cached.conn;
+// // }
+
+// // export default connectToDatabase;
+
+//new code add by me
+
+import mongoose, { Connection } from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-let cached = (global as any).mongoose || { conn: null, promise: null };
+interface Cached {
+  conn: Connection | null;
+  promise: Promise<typeof mongoose> | null;
+}
+
+let cached: Cached = {
+  conn: null,
+  promise: null,
+};
 
 export const connectToDatabase = async () => {
-  if (cached.conn) return cached.conn;
+  if (cached.conn) {
+    return cached.conn;
+  }
 
-  if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
+  if (!MONGODB_URI) {
+    throw new Error("MONGODB_URI is missing");
+  }
 
-  cached.promise =
-    cached.promise ||
-    mongoose.connect(MONGODB_URI, {
-      bufferCommands: false,
-    });
+  try {
+    cached.promise =
+      cached.promise ||
+      mongoose.connect(MONGODB_URI, {
+        // dbName: "EventApp",
+        bufferCommands: false,
+      });
 
-  cached.conn = await cached.promise;
+    const mongooseInstance = await cached.promise;
+    cached.conn = mongooseInstance.connection;
 
-  return cached.conn;
+    return cached.conn;
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw new Error("Unable to connect to MongoDB");
+  }
 };
